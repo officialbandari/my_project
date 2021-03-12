bc58b2b09bc961b2f2f4d093f68edf01bb17bf61
krishna <officialbandari6@gmail.com>
2021-03-12 09:08:05 -0800

    log commit

---
+++ ch_index.go
@@ -0,0 +1,33 @@
+package main 
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	ch := make(chan int,50)
+	wg.Add(2)
+
+	go func(ch chan<- int){
+		for{
+			if i,ok := <-ch;ok{
+				fmt.Println(i)
+			}else{
+				break
+			}
+		}
+		wg.Done()
+	}(ch)
+
+	go func(ch <-chan int){
+		ch <-23
+		ch <-33
+		close(ch)
+		wg.Done()
+
+	}(ch)
+	wg.Wait()
+
+}
