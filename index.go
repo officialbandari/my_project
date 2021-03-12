8079d290cba5114850db1db31ebce5a510fec9ec
krishna <officialbandari6@gmail.com>
2021-03-12 08:03:29 -0800

    fifth commit

--- index.go
+++ index.go
@@ -1,4 +1,5 @@
 package main 
+
 import (
 	"fmt"
 	"sync"
@@ -7,26 +8,24 @@ import (
 var wg = sync.WaitGroup{}
 
 func main() {
-
-	ch := make(chan int,50)
+	ch := make(chan int, 50)
 	wg.Add(2)
-
-	go func(ch <-chan int)  {
-		i:= <-ch
-		fmt.Println(i)
-		i = <-ch
-		fmt.Println(i)
+	go func(ch <-chan int) {
+		for i := range ch{
+			fmt.Println(i)
+		}
 		wg.Done()
 		
 	}(ch)
 
-	go func(ch chan<- int)  {
-		ch <- 33
-		ch <- 44
+	go func(ch chan<- int) {
+		ch <-22
+		ch <-33
+		close(ch)
 		wg.Done()
 		
 	}(ch)
+
 	wg.Wait()
-
+	
 }
-
